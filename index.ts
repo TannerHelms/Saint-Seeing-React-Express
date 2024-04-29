d51c60c9f43e52603d6af83aa62e41130f3e2044
TannerHelms <tannerhelms01@gmail.com>
2024-04-29 13:35:55 -0600

    https

--- index.ts
+++ index.ts
@@ -31,15 +31,11 @@ export const DEBUG = process.env.NODE_ENV !== "production";
 export const MANIFEST: Record<string, any> = DEBUG ? {} : JSON.parse(fs.readFileSync("static/.vite/manifest.json").toString())
 
 const cors = require('cors');
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  optionsSuccessStatus: 200,
-}
 const app = express();
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(fileUpload());
 
 app.use(bodyParser.json());
@@ -70,13 +66,17 @@ app.post("/upload", (req, res) => {
   });
 });
 
-const httpsServer = https.createServer(app);
-httpsServer.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}...`);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+}, app);
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
 });
 
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Listening on port ${process.env.PORT || 3000}...`);
-// });
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT || 3000}...`);
+});
 
 
